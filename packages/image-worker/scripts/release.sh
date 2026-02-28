7645f3048bb38c0d3a7fd053c0f289f71ec28920
Sivert Gullberg Hansen <sivertgullberg@gmail.com>
2026-02-28 19:24:00 +0100

    chore: update image-worker version to 1.0.3-beta and enhance Docker build caching in release script

--- packages/image-worker/scripts/release.sh
+++ packages/image-worker/scripts/release.sh
@@ -202,6 +202,8 @@ info "Building & pushing multi-arch Docker image (${PLATFORMS})…"
 
 docker buildx build \
   --platform "$PLATFORMS" \
+  --cache-from type=registry,ref=${IMAGE}:buildcache \
+  --cache-to type=registry,ref=${IMAGE}:buildcache,mode=max \
   -t "${IMAGE}:${NEW_VERSION}" \
   -t "${IMAGE}:${V_MAJOR}.${V_MINOR}" \
   -t "${IMAGE}:${V_MAJOR}" \
